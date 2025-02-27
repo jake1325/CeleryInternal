4a27ea2fb60a11c4768c217c9312c0a7567e8515
void <jake52123@proton.me>
2025-02-27 21:15:15 +0900

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+--/ discontinued!!!
+--/ discontinued!!!
+
+print("Project celeryinternal has been discontinued. Thanks for supporting us!")
+print("RIP 2024-2025 CeleryInternal")
+print("Made by idk someone")
